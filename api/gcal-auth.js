08431860d863e3e817b415d9a72fd3510ebdb629
ernst-law-office <ernstadv@gmail.com>
2026-03-26 17:41:08 +0200

    Update gcal-auth.js

--- api/gcal-auth.js
+++ api/gcal-auth.js
@@ -2,6 +2,6 @@ module.exports = function handler(req, res) {
   const clientId = process.env.GOOGLE_CLIENT_ID;
   const redirectUri = 'https://ernst-law-ten.vercel.app/api/gcal-callback';
   const scope = encodeURIComponent('https://www.googleapis.com/auth/calendar');
-  const authUrl = `https://accounts.google.com/o/oauth2/auth?client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&response_type=code&scope=${scope}&access_type=offline&prompt=consent`;
+  const authUrl = `https://accounts.google.com/o/oauth2/auth?client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&response_type=code&scope=${scope}&access_type=offline&prompt=consent&login_hint=office%40ernstadv.com`;
   res.redirect(authUrl);
 }
